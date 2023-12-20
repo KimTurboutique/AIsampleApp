43a933ff8e65467143c9f9e4f42695d7d11e8cb6
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-12-20 14:13:10 -0500

    instantiated openAI, called model and user input

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
+const { OpenAI } = require('langchain/llms/openai');
+const inquirer = require('inquirer')
 require('dotenv').config();
-console.log(process.env.OPENAI_API_KEY);
+require('web-streams-polyfill');
+
+const model = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+});
+
+console.log({ model });
+
+const promptFunc = async (input) => {
+    try {
+      const res = await model.call(input);
+      console.log(res);
+    }
+    catch (err) {
+      console.error(err);
+    }
+
+};
+
+const init = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Ask a coding question:',
+      },
+    ]).then((inquirerResponse) => {
+      promptFunc(inquirerResponse.name)
+    });
+  };
+init();
